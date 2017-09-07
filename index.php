5b51799c90ae1070d13a1472bd058f58aa6cdd0b
Judy-Zhu <zhudan_judy@aliyun.com>
2017-09-07 14:02:37 -0700

    Added Student information

--- index.php
+++ index.php
@@ -11,8 +11,40 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "I'm so excited to learn how to develop webapps in COMP4711!<br>";
-        echo 'Woohoo!';
+        include('Student.php');
+        
+        #echo "I'm so excited to learn how to develop webapps in COMP4711!<br>";
+        #echo 'Woohoo!';
+        
+        $students = array();
+        
+        # first student information
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        # second student information
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+
+        foreach($students as $student)
+            echo $student->toString();
+        
         ?>
     </body>
 </html>
